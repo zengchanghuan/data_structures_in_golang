8bc7a3eca259bee4c198435d4bbb6b4c9ec89d68
ZengChanghuan <zengchanghuan@gmail.com>
2021-08-17 21:37:08 +0800

    add CircularQueue interface

--- queue/cycleQueue/cycleQueue.go
+++ queue/cycleQueue/cycleQueue.go
@@ -1,43 +1,49 @@
 package main
+/*
+为充分利用向量空间，克服"假溢出"现象的方法是：
+将向量空间想象为一个首尾相接的圆环，并称这种向量为循环向量。
+存储在其中的队列称为循环队列（Circular Queue）。
+循环队列是把顺序队列首尾相连，把存储队列元素的表从逻辑上看成一个环，成为循环队列。
 
-type node struct {
-	data int
-	pre  *node
-	next *node
-}
-
-type Queue struct {
-	head *node //头指针
-	tail *node //尾指针
-	size int
-}
-
-func (q *Queue) Enqueue(e int) {
-
-}
-func (q *Queue) Empty() bool {
-	return q.head == q.tail
-}
-func (q *Queue) Dequeue() int {
+*/
+type MyCircularQueue struct {
 
 }
 
-func (q *Queue) Peek() int {
-	if q.Empty() {
-		return -1
-	}
-	return q.head.next.data
+func Constructor(k int) MyCircularQueue {
+
 }
 
-func (q *Queue) Size() int {
-	return q.size
+
+func (this *MyCircularQueue) EnQueue(value int) bool {
+
 }
 
-func (q *Queue) Clear() {
-	q.head.next = nil
-	q.tail = q.head
-	q.size = 0
+
+func (this *MyCircularQueue) DeQueue() bool {
+
 }
-func main() {
+
+
+func (this *MyCircularQueue) Front() int {
+
+}
+
+
+func (this *MyCircularQueue) Rear() int {
+
+}
+
+
+func (this *MyCircularQueue) IsEmpty() bool {
+
+}
+
+
+func (this *MyCircularQueue) IsFull() bool {
+
+}
+
+func main()  {
 
 }
